00c249ab6b8db198d5cb6a35c44e85a495cc706a
arushiahmed <35859311+arushiahmed@users.noreply.github.com>
2019-02-25 16:44:36 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,1 +1,9 @@
+document.getElementById("add").onClick = function() {
+
+    var text = document.getElementById("myText").value;
+
+    var li = "<li>" + text + </li>;
+
+    document.getElementById("list").appendChild(li);
+}
 
